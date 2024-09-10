e9ef6915ded84d9c0b985be900eb2f68a7f2f2fb
srxdex <boyzdex@gmail.com>
2024-09-10 14:25:12 -0300

    mod main

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ in_pokemon = sys.argv[1]
 
 pokemon = pb.pokemon(in_pokemon)
 print(pokemon.name) 
-print(pokemon.height)
+#print(pokemon.height)
 sprite = pokemon.sprites.front_default
 print(sprite)
 
